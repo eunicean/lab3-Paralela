8db6b5349ff28aa072ca69bb38b0b930a39e3279
eunicean <eunice.mata2992@gmail.com>
2025-10-01 18:04:34 -0600

    parte 2

--- anticipada.c
+++ anticipada.c
@@ -13,21 +13,12 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size != 2) {
-        if (rank == 0) {
-            printf("Este programa requiere exactamente 2 procesos.\n");
-        }
-        MPI_Finalize();
-        return 0;
-    }
-
     if (rank == 0) {
         number = 1234;
         MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     } else {
         MPI_Irecv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
 
-        // Mientras esperamos, hacemos operaciones
         for (int i = 0; i < 1000000; i++) {
             result += i % 5;
             MPI_Test(&request, &flag, &status);
--- pipeline.c
+++ pipeline.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define CHUNK 1000
-#define N 10000  // tamaño total del arreglo
+#define N 10000
 
 int main(int argc, char** argv) {
     int rank, size;
@@ -15,15 +15,6 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if (size != 2) {
-        if (rank == 0) {
-            printf("Este programa requiere exactamente 2 procesos.\n");
-        }
-        MPI_Finalize();
-        return 0;
-    }
-
     if (rank == 0) {
         // llenar datos
         for (int i = 0; i < N; i++) data[i] = i;
@@ -38,7 +29,6 @@ int main(int argc, char** argv) {
         while (1) {
             MPI_Recv(buffer, CHUNK, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (buffer[0] == -1) break; // señal de stop
-            // procesar
             printf("Proceso 1 procesó chunk con inicio %d\n", buffer[0]);
         }
     }
