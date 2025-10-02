f865b874548e3a6013089d1cd583af4f3e2c55e1
eunicean <eunice.mata2992@gmail.com>
2025-10-01 18:31:25 -0600

    agregado ingreso del tamaño del chunk

--- pipeline.c
+++ pipeline.c
@@ -2,37 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CHUNK 1000
 #define N 10000
 
 int main(int argc, char** argv) {
     int rank, size;
-    int data[N];
-    int buffer[CHUNK];
+    int *data = NULL;
+    int *buffer;
     int stop = -1;
     MPI_Request request;
+    double start, end;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int chunk = 1000;
+    if (argc > 1) {
+        chunk = atoi(argv[1]);
+    }
+
+    buffer = malloc(chunk * sizeof(int));
+
     if (rank == 0) {
-        // llenar datos
+        data = malloc(N * sizeof(int));
         for (int i = 0; i < N; i++) data[i] = i;
 
-        for (int i = 0; i < N; i += CHUNK) {
-            MPI_Send(&data[i], CHUNK, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        start = MPI_Wtime();
+
+        for (int i = 0; i < N; i += chunk) {
+            MPI_Send(&data[i], chunk, MPI_INT, 1, 0, MPI_COMM_WORLD);
         }
 
-        // enviar stop
+        // enviar señal de stop
         MPI_Isend(&stop, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+
+        end = MPI_Wtime();
+        printf("Chunk size: %d, Tiempo total: %f segundos\n", chunk, end - start);
+
+        free(data);
     } else {
         while (1) {
-            MPI_Recv(buffer, CHUNK, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if (buffer[0] == -1) break; // señal de stop
+            MPI_Recv(buffer, chunk, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (buffer[0] == -1) break;
             printf("Proceso 1 procesó chunk con inicio %d\n", buffer[0]);
         }
     }
 
+    free(buffer);
     MPI_Finalize();
     return 0;
 }
